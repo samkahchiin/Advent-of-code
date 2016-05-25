6e465232f9685db9fbcebc67b6816d5b0b7e115f
samkahchiin <samkahchiin@gmail.com>
2016-05-26 07:25:36 +0800

    Refactor the code

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ##--- Day 1: Not Quite Lisp ---
 
-###--- PART 1 ---
+###--- Part One ---
 
 Santa was hoping for a white Christmas, but his weather machine's "snow" function is powered by stars, and he's fresh out! To save Christmas, he needs you to collect fifty stars by December 25th.
 
--- solution.rb
+++ solution.rb
@@ -1,28 +1,28 @@
-# Part 1
-instructions = File.read('input.txt')
-floor = 0
-instructions.split("").each_with_index do |instruction, index|
-  case instruction
-  when '('
-    floor += 1
-  when ')'
-    floor -= 1
+class SantaTracker
+  def initialize file
+    @instructions = File.read file
   end
-end
-puts "Part 1: the instructions take Santa to #{floor} floor"
 
-floor = 0
-position = 0
-instructions.split("").each_with_index do |instruction, index|
-  case instruction
-  when '('
-    floor += 1
-  when ')'
-    floor -= 1
-  end
-  if floor == -1
-    position = index + 1
-    break
+  def track
+    floor = 0
+    position = 0
+    count = 0
+    @instructions.split("").each_with_index do |instruction, index|
+      case instruction
+      when '('
+        floor += 1
+      when ')'
+        floor -= 1
+      end
+      if floor == -1 && count == 0
+        position = index + 1
+        count += 1
+      end
+    end
+    puts "Part 1: the instructions take Santa to #{floor} floor"
+    puts "Part 2: The position of the character that causes Santa to first enter the basement is #{position}"
   end
 end
-puts "Part 2: The position of the character that causes Santa to first enter the basement is #{position}"
+
+santa_tracker = SantaTracker.new 'input.txt'
+santa_tracker.track
