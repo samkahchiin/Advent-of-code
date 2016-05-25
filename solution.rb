860f4689eb74273346d183543b84ecac6d4b8123
samkahchiin <samkahchiin@gmail.com>
2016-05-25 23:10:08 +0800

    WIP - Answer for Part 2

--- solution.rb
+++ solution.rb
@@ -1,13 +1,28 @@
-
-contents = File.read('input.txt')
-count = 0
-contents.split("").each do |x|
-  case x
+# Part 1
+instructions = File.read('input.txt')
+floor = 0
+instructions.split("").each_with_index do |instruction, index|
+  case instruction
   when '('
-    count += 1
+    floor += 1
   when ')'
-    count -= 1
+    floor -= 1
   end
 end
+puts "Part 1: the instructions take Santa to #{floor} floor"
 
-puts count
+floor = 0
+position = 0
+instructions.split("").each_with_index do |instruction, index|
+  case instruction
+  when '('
+    floor += 1
+  when ')'
+    floor -= 1
+  end
+  if floor == -1
+    position = index + 1
+    break
+  end
+end
+puts "Part 2: The position of the character that causes Santa to first enter the basement is #{position}"
