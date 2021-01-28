19e0b668fe8b684b6523a1710f8e3a66bdaf3132
samkahchiin <kcsam@siliconjungles.com>
2021-01-28 12:55:16 +0800

    :sparkles: Update elixir day 1 solution
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- 2015/day_1/elixir.exs
+++ 2015/day_1/elixir.exs
@@ -15,3 +15,20 @@ end
 IO.puts("Santa Claus is at level #{position}")
 
 
+position_tuple = case File.read("input.txt") do
+    {:ok, body} ->
+    body
+    |> String.split(~r{\B}, trim: true)
+    |> Enum.reduce_while({0, 0}, fn(direction, {idx, floor}) -> 
+        floor = case direction do
+            "(" -> floor + 1
+            ")" -> floor - 1
+            _ -> floor
+        end
+        if floor == -1, do: {:halt, {idx + 1, floor}}, else: {:cont, {idx + 1, floor}}
+    end)
+end
+
+{idx, _} = position_tuple
+
+IO.puts("Part 2: The position of the character that causes Santa to first enter the basement is #{idx}")
