830f82702de8404fa346c3f55b5d93a626c39988
samkahchiin <kcsam@siliconjungles.com>
2021-08-22 23:04:18 +0800

    :sparkles: Finish 2015 day 9 using ruby
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- 2015/day_9/solution.rb
+++ 2015/day_9/solution.rb
@@ -1,34 +1,58 @@
 require 'byebug'
 
+## Using brute-force and permutations
 class File
-  attr_accessor :contents
+  INSTRUCTION_REGEXP = /(\w+) to (\w+) = (\d+)/
+  attr_reader :instructions
 
-  def initialize file_name
+  def initialize(file_name)
+    @instructions = []
     @contents = File.read(file_name).split("\n")
   end
-end
 
-class RoadPlanner
-  def initialize data_arr
-    @directory = {}
-    place_arr = []
-    data_arr.each do |data|
-      data = data.split(' ')
-      unless @directory[data[0]]
-        inner_hash = {}
-        inner_hash[data[2]] = data[4]
-        @directory[data[0]] = inner_hash
-      else
-        @directory[data[0]][data[2]] = data[4]
-      end
-      place_arr << data[2] << data[0]
-    end
-    @places = place_arr.uniq
-  end
-
-  def find_shortest_route
-    @places.each_with_index do |place|
-
+  def generate_instructions
+    @contents.each do |line|
+      info = line.scan(INSTRUCTION_REGEXP)[0]
+      @instructions << info
     end
   end
 end
+
+class Graph
+  attr_reader :nodes, :graph
+
+  def initialize
+    @graph = {}
+  end
+
+  def connect_nodes(from, to, cost)
+    if graph.key?(from)
+      @graph[from][to] = cost
+    else
+      @graph[from] = { to => cost }
+    end
+  end
+
+  def add_edge(from, to, cost)
+    # for undirected graph
+    connect_nodes(from, to, cost)
+    connect_nodes(to, from, cost)
+  end
+
+  def get_shortest_path
+    @graph.keys.permutation(@graph.keys.size).map do |possible_comb|
+      possible_comb.each_cons(2).reduce(0) { |acc, (from, to)| acc + @graph[from][to].to_i }
+    end.max
+  end
+end
+
+file = File.new('input.txt')
+file.generate_instructions
+
+graph = Graph.new
+
+file.instructions.each do |from, to, cost|
+  graph.add_edge(from, to, cost)
+end
+
+puts graph.get_shortest_path
