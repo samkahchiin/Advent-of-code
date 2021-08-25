b726c5c555f142f660124010f00aba13d9fd5b94
samkahchiin <kcsam@siliconjungles.com>
2021-08-25 23:07:47 +0800

    :recycle: Refactor the day_11 code
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- 2015/day_11/ruby/solution.rb
+++ 2015/day_11/ruby/solution.rb
@@ -1,7 +1,7 @@
 class PasswordValidator
-	A_VALUE = "a".ord
-	Z_VALUE = "z".ord
-	CONFUSING_CHARS = ['i', 'o', 'l']
+	A_VALUE = 'a'.ord
+	Z_VALUE = 'z'.ord
+	CONFUSING_CHARS = %w[i o l]
 
 	def initialize(string)
 		@string = string
@@ -10,14 +10,14 @@ class PasswordValidator
 	def generate_next_string
 		int_arr = convert_to_integers(@string)
 		current_idx = -1
-		while true
+		loop do
 			break if int_arr.count < -current_idx
 			unless exceed_z?(int_arr[current_idx])
 				int_arr[current_idx] += 1
 				break
 			end
 
-			if is_confusing_char?(int_arr[current_idx])
+			if confusing_char?(int_arr[current_idx])
 				int_arr[current_idx] += 2
 				break
 			end
@@ -29,13 +29,13 @@ class PasswordValidator
 	end
 
 	def generate_next_password
-		while true
+		loop do
 			@string = generate_next_string
-			return @string if is_valid?
+			return @string if valid?
 		end
 	end
 
-	def is_valid?
+	def valid?
 		no_confusing_chars && validate_overlapping_chars && contain_three_increasing_chars
 	end
 
@@ -50,19 +50,15 @@ class PasswordValidator
 	end
 
 	def contain_three_increasing_chars
-		begin
-			int_arr = convert_to_integers(@string)
-			!int_arr.each_cons(3).find { |a, b, c| a + 1 == b && b + 1 == c }.nil?
-		rescue => e
-			puts @string
-		end
+		int_arr = convert_to_integers(@string)
+		!int_arr.each_cons(3).find { |a, b, c| a + 1 == b && b + 1 == c }.nil?
 	end
 
 	def exceed_z?(char_val)
 		char_val >= Z_VALUE
 	end
 
-	def is_confusing_char?(char_val)
+	def confusing_char?(char_val)
 		CONFUSING_CHARS.map(&:ord).include? char_val
 	end
 
@@ -73,9 +69,6 @@ class PasswordValidator
 	def convert_to_string(int_arr)
 		int_arr.map(&:chr).join
 	end
-
 end
 
-res = PasswordValidator.new("hxbxwxba").generate_next_password
-puts res
-# puts PasswordValidator.new("ghjaabcc").is_valid?
+puts PasswordValidator.new('hxbxxyzz').generate_next_password
